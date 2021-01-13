3ab9801dfda5e8aef598bd4794b143f0e13454d5
confidential-nt <youyoy689@gmail.com>
2021-01-14 01:49:58 +0900

    made Deck obj

---
+++ assets/deck.js
@@ -0,0 +1,43 @@
+const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const colors = ["black", "red", "blue", "orange"];
+
+export default class Deck {
+  constructor(deck = this.createDeck()) {
+    this.deck = deck;
+  }
+
+  createNumberTile() {
+    return values.flatMap((val) => {
+      return colors.map((col) => {
+        return new Tile(val, col);
+      });
+    });
+  }
+
+  get numberOfTiles() {
+    return this.deck.length;
+  }
+
+  createDeck() {
+    const numberTile = this.createNumberTile();
+    const JOKER = [new Tile("J", "red"), new Tile("J", "black")];
+    return numberTile.concat(numberTile).concat(JOKER);
+  }
+
+  shuffle() {
+    for (let i = this.numberOfTiles - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * this.numberOfTiles);
+      let targetTile = this.deck[i];
+      let otherTile = this.deck[newIndex];
+      this.deck[i] = otherTile;
+      this.deck[newIndex] = targetTile;
+    }
+  }
+}
+
+class Tile {
+  constructor(value, color) {
+    this.value = value;
+    this.color = color;
+  }
+}
---
+++ assets/script.js
@@ -0,0 +1,5 @@
+import Deck from "./deck.js";
+
+let a = new Deck();
+a.shuffle();
+console.log(a.deck[0]);
